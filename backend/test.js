8d176d54b13039eaa10f33b6437bed954d066a40
RoMan-robots <romkozhan@gmail.com>
2023-11-09 22:25:58 +0200

    updated test.js

--- backend/test.js
+++
@@ -1,7 +0,0 @@
-// Генерує випадкове число 0 або 1
-const random = Math.floor(Math.random() * 2);
-
-// Визначає, чи це "так" чи "ні" на основі випадкового числа
-const answer = random === 1 ? 'так' : 'ні';
-
-console.log(answer);
