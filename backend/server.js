0a60d6b8ecdc1bc2ad8ef7e84ddfac94ebf6267f
RoMan-robots <romkozhan@gmail.com>
2023-11-10 13:16:06 +0200

    back

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
 app.get("/login.html", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/html", "login.html"));
 });
+
 app.get("/test.html", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/html", "test.html"));
 })
