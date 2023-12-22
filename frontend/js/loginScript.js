0ef67aaccfe8be4ecd4630a9a64808f01949129d
RoMan-robots <romkozhan@gmail.com>
2023-12-22 20:06:52 +0200

    solved problem vith dropdown

--- frontend/js/loginScript.js
+++ frontend/js/loginScript.js
@@ -1,7 +1,3 @@
-const loginScreen = document.getElementById('login-screen');
-const loginForm = document.getElementById('login-form');
-const usernameInput = document.getElementById('username-input');
-const passwordInput = document.getElementById('password-input');
 const loginButton = document.getElementById('login-button');
 
 let usersLogins = [
@@ -12,37 +8,31 @@ let usersLogins = [
     "123"
   ];
   
-  
-
-  function login() {
-    const enteredUsername = usernameInput.value;
-    const enteredPassword = passwordInput.value;
-  
-    const isValidLogin = usersLogins.includes(enteredUsername);
-    const isValidPassword = usersPasswords[usersLogins.indexOf(enteredUsername)] === enteredPassword;
-    while (True){
-      if (isValidLogin && isValidPassword) {
-        username = enteredUsername;
-        displayWelcomeMessage(username);
-        break;
-        } else if (isValidLogin && !isValidPassword) {
-        alert('Неправильний пароль. Будь ласка, перевірте ваш пароль та спробуйте знову.');
-  
-        } else {
-          alert('Такого аккаунту не існує. Будь ласка, перевірте ваші дані входу.');
-        }
-      }
-    }
-  
-  function displayWelcomeMessage(username) {
-    const welcomeMessage = `Ласкаво просимо, ${username}! Ви успішно увійшли до RoMan Talk.`;
-    displayMessage(welcomeMessage);
-  }
-  
   function changeUrlToChat(url) {
     window.location.href = url;
   }
+
+  function login() {
+    const enteredUsername = document.getElementById('username-input').value;
+    const enteredPassword = document.getElementById('password-input').value;
   
+    const isValidLogin = usersLogins.includes(enteredUsername);
+    const isValidPassword = usersPasswords[usersLogins.indexOf(enteredUsername)] === enteredPassword;
+  
+    if (isValidLogin && isValidPassword) {
+      username = enteredUsername;
+      displayWelcomeMessage(username);
+    } else if (isValidLogin && !isValidPassword) {
+      alert('Неправильний пароль. Будь ласка, перевірте ваш пароль та спробуйте знову.');
+    } else {
+      alert('Такого аккаунту не існує. Будь ласка, перевірте ваші дані входу.');
+    }
+  }
+  
+  export function displayWelcomeMessage(username) {
+    const welcomeMessage = `Ласкаво просимо, ${username}! Ви успішно увійшли до RoMan Talk.`;
+    displayMessage(welcomeMessage);
+  }
   
   loginButton.addEventListener('click', login);
   
