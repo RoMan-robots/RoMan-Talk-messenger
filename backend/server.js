75a8d366aea72ca88379aa930e77ae6f3717f188
RoMan-robots <romkozhan@gmail.com>
2023-12-26 15:19:36 +0200

    server update ip

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,6 @@ app.get('/favicon.ico', (req, res) => {
   res.status(204).end();
 });
 
-app.listen(port, '192.168.0.2', () => {
-  console.log(`Server is running on port ${port}. Test at: http://192.168.0.2:${port}/`);
+app.listen(port, '127.0.0.1', () => {
+  console.log(`Server is running on port ${port}. Test at: http://127.0.0.1:${port}/`);
 });
