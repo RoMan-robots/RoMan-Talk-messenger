1eb59530fcc83950daf8665e2c4daf366e95ce9f
RoMan-robots <romkozhan@gmail.com>
2024-02-06 20:34:38 +0200

    1.1 beta 2

--- frontend/js/registerScript.js
+++ frontend/js/registerScript.js
@@ -1,40 +1,45 @@
-const loginScreenButton = document.getElementById('login-screen-button');
-const registerScreenButton = document.getElementById('register-screen-button');
-const helloScreen = document.getElementById('hello-screen');
-
+function changeUrlToChat(url) {
+  window.location.href = url;
+}
 console.log("Привіт! Це консоль для розробників, де виводяться різні помилки. Якщо ти звичайний користувач, який не розуміє, що це таке, краще вимкни це вікно та нічого не крути.")
+async function register(event) {
+  event.preventDefault();
+  const registerUsernameInput = document.getElementById('register-username-input');
+  const registerPasswordInput = document.getElementById('register-password-input');
+  const enteredPasswordDuplicateInput = document.getElementById('dublicate-register-password-input');
 
-function changeUrlToLogin(url) {
-    window.location.href = url;
-  }
-  function changeUrlToRegister(url) {
-    window.location.href = url;
+  const enteredUsername = registerUsernameInput.value;
+  const enteredPassword = registerPasswordInput.value;
+  const enteredPasswordDuplicate = enteredPasswordDuplicateInput.value;
+
+  if (enteredPassword !== enteredPasswordDuplicate) {
+    alert('Пароль не співпадає');
+    return;
   }
 
-  async function checkSessionAndRedirect() {
-    try {
-      const response = await fetch('/check-session');
-      const data = await response.json();
-  
-      if (data.isLoggedIn && ['/', '/login', '/register'].includes(window.location.pathname)) {
-        window.location.href = '/chat.html';
-      }
-    } catch (error) {
-      console.error('Error:', error);
+  try {
+    const response = await fetch('/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: enteredUsername, password: enteredPassword })
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      changeUrlToChat('chat.html');
+    } else {
+      alert(data.message || 'Помилка реєстрації.');
     }
+  } catch (error) {
+    alert('Помилка сервера при реєстрації');
   }
-  
-  async function checkSessionStatus() {
-    try {
-      const response = await fetch('/session-status');
-      const data = await response.json();
-  
-      if (data.loggedIn) {
-        window.location.href = '/chat.html';
-      }
-    } catch (error) {
-      console.error('Помилка при перевірці статусу сесії:', error);
-    }}
+}
 
-  document.addEventListener("DOMContentLoaded", checkSessionAndRedirect, checkSessionStatus);
-  
+if (document.cookie.indexOf('isLoggedIn=true') !== -1) {
+  if (['/', '/login', '/register'].includes(window.location.pathname)) {
+    window.location.href = '/chat.html';
+  }
+}
