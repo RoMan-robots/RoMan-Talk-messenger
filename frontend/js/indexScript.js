08d5c0f9ef416138d3b8eda4f36d59db0305102e
RoMan-robots <romkozhan@gmail.com>
2024-03-29 17:13:28 +0200

    tos update

--- frontend/js/indexScript.js
+++ frontend/js/indexScript.js
@@ -8,6 +8,7 @@ function changeUrlToLogin(url) {
   function changeUrlToRegister(url) {
     window.location.href = url;
   }
+    
   async function checkSessionStatus() {
     try {
       const response = await fetch('/session-status');
@@ -15,6 +16,8 @@ function changeUrlToLogin(url) {
   
       if (data.loggedIn) {
         window.location.href = '/chat.html';
+        
+        
       }
     } catch (error) {
       console.error('Помилка при перевірці статусу сесії:', error);
