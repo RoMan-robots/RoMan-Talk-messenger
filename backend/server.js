c69b7fb727074cc73a0add8a89240e22a4dcd59d
RoMan-robots <131284689+RoMan-robots@users.noreply.github.com>
2024-09-03 09:43:11 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1701,19 +1701,7 @@ app.get("/settings.html", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/html", "settings.html"));
 });
 
-httpServer.listen(port, 'localhost', () => {
-    fs.readdir(imagesDir, (err, files) => {
-        if (err) {
-            console.error('Unable to scan directory:', err);
-            return;
-        }
-
-        shuffledImages = shuffleArray(files);
-    });
-    console.log(`Server is running on port ${port}. Test at: http://localhost:${port}/`);
-});
-
-// httpServer.listen(port, () => {
+// httpServer.listen(port, 'localhost', () => {
 //     fs.readdir(imagesDir, (err, files) => {
 //         if (err) {
 //             console.error('Unable to scan directory:', err);
@@ -1722,6 +1710,18 @@ httpServer.listen(port, 'localhost', () => {
 
 //         shuffledImages = shuffleArray(files);
 //     });
+//     console.log(`Server is running on port ${port}. Test at: http://localhost:${port}/`);
+// });
 
-//     console.log(`App listening on port ${port}!`)
-// }); 
+httpServer.listen(port, () => {
+    fs.readdir(imagesDir, (err, files) => {
+        if (err) {
+            console.error('Unable to scan directory:', err);
+            return;
+        }
+
+        shuffledImages = shuffleArray(files);
+    });
+
+    console.log(`App listening on port ${port}!`)
+}); 
